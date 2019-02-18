c98eae6cac54227dbae043fb93e641fbb7a05973
wangyun <wangyun@yusys.com.cn>
2019-02-18 16:27:41 +0800

    提交

--- comet-mybatis-generator/src/test/java/com/dcits/comet/mybatis/generator/Main.java
+++ comet-mybatis-generator/src/test/java/com/dcits/comet/mybatis/generator/Main.java
@@ -3,6 +3,7 @@ package com.dcits.comet.mybatis.generator;
 import com.dcits.comet.mybatis.generator.mapper.FreemarkerUtil;
 import com.dcits.comet.mybatis.generator.mapper.SqlModule;
 import com.dcits.comet.mybatis.generator.showcase.entity.SysLog;
+import yunyun.entity.CifClientPo;
 
 import java.io.File;
 import java.util.HashMap;
@@ -13,14 +14,14 @@ public class Main {
 	public static void main(final String[] args) {
 		final File f = new File(Main.class.getResource("/").getPath());
 		final String path = f.getPath();
-		final SqlModule sqlModule = new SqlModule(SysLog.class);
+		final SqlModule sqlModule = new SqlModule(CifClientPo.class);
 		// 表名
-		sqlModule.setTableName("SysLog");
+		sqlModule.setTableName("CifClientPo");
 		final FreemarkerUtil freemarkerUtil = new FreemarkerUtil();
 		final Map<String, Object> module = new HashMap<>();
 		module.put("module", sqlModule);
 		System.out.println(path);
-		freemarkerUtil.fprint("mapper.ftl", module, path + "/resources/", "SysLogMapper.xml");
+		freemarkerUtil.fprint("mapper.ftl", module, path + "/resources/", "CifClientMapper.xml");
 	}
 
 }
