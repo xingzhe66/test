38ea13c014ff63cacaef2b37276d34bf8d3cb928
wangyun <wangyun@yusys.com.cn>
2019-02-28 14:41:51 +0800

    修改batch配置方式，增加注解配置；

---
+++ comet-batch/src/main/java/com/dcits/comet/batch/AbstractTaskletStep.java
@@ -0,0 +1,23 @@
+package com.dcits.comet.batch;
+
+import com.dcits.comet.batch.exception.BatchException;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.repeat.RepeatStatus;
+
+public abstract class AbstractTaskletStep implements ITaskletStep{
+
+    @Override
+    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+
+        try {
+            this.exe();
+        } catch (Throwable throwable) {
+            throw new BatchException("批量执行错误！错误信息："+throwable.getMessage());
+        }
+
+        return RepeatStatus.FINISHED;
+    }
+
+    public abstract void exe() throws Throwable;
+}
