adf5fa4f59ca9ce116f4229e391e790931a93a5b
leijian <leijian@dcits.com>
2019-04-02 16:01:05 +0800

    Java bean 修改

---
+++ comet-sequence/comet-sequence-provider/src/main/java/com/dcits/comet/uid/provider/service/UidGeneratorConfig.java
@@ -0,0 +1,86 @@
+package com.dcits.comet.uid.provider.service;
+
+import com.dcits.comet.uid.impl.DefaultUidGenerator;
+import com.dcits.comet.uid.impl.LoadingUidGenerator;
+import com.dcits.comet.uid.impl.RedisUidGenerator;
+import com.dcits.comet.uid.worker.DisposableWorkerIdAssigner;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+/**
+ * @author leijian
+ * @version 1.0
+ * @date 2019/4/2 15:52
+ **/
+@Slf4j
+@Component
+public class UidGeneratorConfig {
+    @Bean("workerIdAssigner")
+    @DependsOn("ds_uid")
+    public DisposableWorkerIdAssigner disposableWorkerIdAssigner(@Autowired DataSource dataSource) {
+        DisposableWorkerIdAssigner disposableWorkerIdAssigner = new DisposableWorkerIdAssigner();
+        disposableWorkerIdAssigner.setDataSource(dataSource);
+        return disposableWorkerIdAssigner;
+    }
+
+    @Bean
+    @DependsOn("workerIdAssigner")
+    @ConditionalOnClass(RedisTemplate.class)
+    @ConditionalOnBean(DisposableWorkerIdAssigner.class)
+    public RedisTemplate<String, Object> redisTemplate(@Autowired RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashKeySerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+
+    @Bean
+    @DependsOn("workerIdAssigner")
+    @ConditionalOnBean({RedisTemplate.class, DisposableWorkerIdAssigner.class})
+    public RedisUidGenerator redisUidGenerator(@Qualifier("workerIdAssigner") DisposableWorkerIdAssigner workerIdAssigner, @Autowired RedisTemplate redisTemplate) {
+        return new RedisUidGenerator(workerIdAssigner, redisTemplate);
+    }
+
+    @Bean
+    @DependsOn("workerIdAssigner")
+    @ConditionalOnBean({DisposableWorkerIdAssigner.class})
+    public LoadingUidGenerator loadingUidGenerator(@Qualifier("workerIdAssigner") DisposableWorkerIdAssigner workerIdAssigner) {
+        return new LoadingUidGenerator(workerIdAssigner);
+    }
+
+    @Bean
+    @DependsOn("workerIdAssigner")
+    @ConditionalOnBean({DisposableWorkerIdAssigner.class})
+    public DefaultUidGenerator defaultUidGenerator(@Qualifier("workerIdAssigner") DisposableWorkerIdAssigner workerIdAssigner) {
+        workerIdAssigner.buildWorkerNode(DefaultUidGenerator.class.getSimpleName().toLowerCase());
+        return new DefaultUidGenerator(workerIdAssigner);
+    }
+
+}
--- comet-sequence/comet-sequence-provider/src/test/java/com/dcits/comet/uid/Application.java
+++ comet-sequence/comet-sequence-provider/src/test/java/com/dcits/comet/uid/Application.java
@@ -1,28 +1,8 @@
 package com.dcits.comet.uid;
 
-import com.dcits.comet.uid.impl.DefaultUidGenerator;
-import com.dcits.comet.uid.impl.LoadingUidGenerator;
-import com.dcits.comet.uid.impl.RedisUidGenerator;
-import com.dcits.comet.uid.worker.DisposableWorkerIdAssigner;
-import com.dcits.comet.uid.worker.WorkerIdAssigner;
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.DependsOn;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-
-import javax.sql.DataSource;
 
 /**
  * @author leijian
@@ -37,58 +17,6 @@ public class Application {
     }
 
 
-    @Bean("workerIdAssigner")
-    @DependsOn("dataSource")
-    public DisposableWorkerIdAssigner disposableWorkerIdAssigner(@Autowired DataSource dataSource) {
-        DisposableWorkerIdAssigner disposableWorkerIdAssigner = new DisposableWorkerIdAssigner();
-        disposableWorkerIdAssigner.setDataSource(dataSource);
-        return disposableWorkerIdAssigner;
-    }
-
-    @Bean
-    @DependsOn("workerIdAssigner")
-    @ConditionalOnClass(RedisTemplate.class)
-    @ConditionalOnBean(DisposableWorkerIdAssigner.class)
-    public RedisTemplate<String, Object> redisTemplate(@Autowired RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
-        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
-        ObjectMapper om = new ObjectMapper();
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        jackson2JsonRedisSerializer.setObjectMapper(om);
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        redisTemplate.setKeySerializer(jackson2JsonRedisSerializer);
-        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
-        redisTemplate.setHashKeySerializer(jackson2JsonRedisSerializer);
-        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
-        redisTemplate.afterPropertiesSet();
-
-        StringRedisTemplate template = new StringRedisTemplate();
-        template.setConnectionFactory(redisConnectionFactory);
-        return redisTemplate;
-    }
-
-    @Bean
-    @DependsOn("workerIdAssigner")
-    @ConditionalOnBean({RedisTemplate.class, DisposableWorkerIdAssigner.class})
-    public RedisUidGenerator redisUidGenerator(@Qualifier("workerIdAssigner") DisposableWorkerIdAssigner workerIdAssigner, @Autowired RedisTemplate redisTemplate) {
-        return new RedisUidGenerator(workerIdAssigner, redisTemplate);
-    }
-
-    @Bean
-    @DependsOn("workerIdAssigner")
-    @ConditionalOnBean({DisposableWorkerIdAssigner.class})
-    public LoadingUidGenerator loadingUidGenerator(@Qualifier("workerIdAssigner") DisposableWorkerIdAssigner workerIdAssigner) {
-        return new LoadingUidGenerator(workerIdAssigner);
-    }
-
-    @Bean
-    @DependsOn("workerIdAssigner")
-    @ConditionalOnBean({DisposableWorkerIdAssigner.class})
-    public DefaultUidGenerator defaultUidGenerator(@Qualifier("workerIdAssigner") DisposableWorkerIdAssigner workerIdAssigner) {
-        workerIdAssigner.buildWorkerNode(DefaultUidGenerator.class.getSimpleName().toLowerCase());
-        return new DefaultUidGenerator(workerIdAssigner);
-    }
 
 
 }
