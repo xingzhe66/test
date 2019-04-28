c78ffc559740e52855992fff1317d61d5b83d801
leijian <leijian@dcits.com>
2019-04-28 18:57:52 +0800

    perf4j版本号

--- comet-sequence/comet-sequence-provider/src/main/java/com/dcits/comet/uid/UidGeneratorApplication.java
+++ comet-sequence/comet-sequence-provider/src/main/java/com/dcits/comet/uid/UidGeneratorApplication.java
@@ -1,6 +1,5 @@
 package com.dcits.comet.uid;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,7 +10,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@Slf4j
 public class UidGeneratorApplication {
     public static void main(String[] args) {
         SpringApplication.run(UidGeneratorApplication.class, args);
