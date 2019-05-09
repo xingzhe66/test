50d21eb76937f7d3cdb6d085509dc43c76e9cb25
wangyun <wangyun@yusys.com.cn>
2019-05-09 16:07:14 +0800

    sonic不稳定，先注释掉

--- comet-batch/comet-batch-sonic/src/test/java/com/dcits/comet/batch/sonic/ServerMain.java
+++ comet-batch/comet-batch-sonic/src/test/java/com/dcits/comet/batch/sonic/ServerMain.java
@@ -1,17 +1,17 @@
-package com.dcits.comet.batch.sonic;
-
-import com.dcits.sonic.executor.SonicExecutorServer;
-
-/**
- * @author leijian
- * @version 1.0
- * @date 2019/5/7 10:14
- **/
-public class ServerMain {
-    public static void main(String[] args) {
-        //获取实例
-        SonicExecutorServer executorServer = SonicExecutorServer.getInstance();
-        //启动
-        executorServer.startup();
-    }
-}
+//package com.dcits.comet.batch.sonic;
+//
+//import com.dcits.sonic.executor.SonicExecutorServer;
+//
+///**
+// * @author leijian
+// * @version 1.0
+// * @date 2019/5/7 10:14
+// **/
+//public class ServerMain {
+//    public static void main(String[] args) {
+//        //获取实例
+//        SonicExecutorServer executorServer = SonicExecutorServer.getInstance();
+//        //启动
+//        executorServer.startup();
+//    }
+//}
