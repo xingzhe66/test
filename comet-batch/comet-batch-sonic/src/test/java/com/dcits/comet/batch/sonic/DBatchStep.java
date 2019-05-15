85379537a8f938023362d8f2ede44f5dd2e41a35
leijian <leijian@dcits.com>
2019-05-15 16:46:19 +0800

    sonic提交

--- comet-batch/comet-batch-sonic/src/test/java/com/dcits/comet/batch/sonic/DBatchStep.java
+++ comet-batch/comet-batch-sonic/src/test/java/com/dcits/comet/batch/sonic/DBatchStep.java
@@ -1,6 +1,7 @@
 package com.dcits.comet.batch.sonic;
 
 import com.dcits.comet.batch.AbstractBStep;
+import com.dcits.comet.batch.helper.JobParameterHelper;
 import com.dcits.comet.batch.holder.JobContextHolder;
 import com.dcits.comet.batch.param.BatchContext;
 import com.dcits.comet.commons.utils.NetUtils;
@@ -18,11 +19,16 @@ import java.util.List;
 @Service("dBatchStep")
 @Slf4j
 public class DBatchStep extends AbstractBStep<WorkerNodePo, WorkerNodePo> {
+    @Override
+    public void preBatchStep(BatchContext batchContext) {
+        log.info("GenPostStep.......preBatchStep");
+        log.info("GenPostStep.......JobId=" + JobParameterHelper.get("jobId"));//从启动参数获取jobId
+    }
 
     @Override
     public List getPageList(BatchContext batchContext, int offset, int pageSize, String node) {
         List list = new ArrayList();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 1; i++) {
             WorkerNodePo workerNodePo = new WorkerNodePo();
             workerNodePo.setId(i);
             workerNodePo.setHostName(NetUtils.getLocalAddress());
