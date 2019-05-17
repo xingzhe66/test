d0ab6735865662026dd6f2323b7e10389f74150f
wangyuna <wangyuna@dcits.com>
2019-05-17 14:33:37 +0800

    新增模块

---
+++ comet-rpc/comet-rpc-provider/src/main/java/com/dcits/comet/rpc/provider/annotation/RpcService.java
@@ -0,0 +1,8 @@
+package com.dcits.comet.rpc.provider.annotation;
+
+import org.springframework.stereotype.Service;
+
+@Service
+//@SofaService(bindings={@SofaServiceBinding(bindingType="bolt")})
+public @interface RpcService {
+}
